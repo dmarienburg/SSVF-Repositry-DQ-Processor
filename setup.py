cd9de4d966c54ba62588df33312a2bf36dcb2333
David Katz-Wigmore <katzwigmore@gmail.com>
2015-07-14 08:20:38 -0700

    Update setup.py
    Fixes syntax issues and updates version number.

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ import py2exe
 
 setup(
     console=[{
-    "script":"RepositoryDQProcessor.py",
-    version="1.1",
-    author="David Katz-Wigmore",
-    name="SSVF DQ Repository Processor",
-    description="A program to facilitate the processing of the monthly SSVF DQ .pdf files")
+        "script": "RepositoryDQProcessor.py",
+        "version": "1.11",
+        "author": "David Katz-Wigmore",
+        "name": "SSVF DQ Repository Processor",
+        "description": "A program to facilitate the processing of the monthly SSVF DQ .pdf files"
+    }
+    ]
+)
