b10460b2b1fdd8f51d64113a6156eded5cf477ca
David Katz-Wigmore <katzwigmore@gmail.com>
2015-07-14 18:41:39 -0700

    Update RepositoryDQProcessor.py
    Modified this script to be object oriented and to dynamically use the providers_list.csv to generate menu options

--- RepositoryDQProcessor.py
+++ RepositoryDQProcessor.py
@@ -2,153 +2,150 @@ __author__ = 'dkatz'
 
 import ttk
 from File_Creator import WriteFile
-from Tkinter import *
+import Tkinter as tk
 
-val_dict = {}
+import Get_Providers
 
-def addition():
-    """
-    Adds a new value to the val_dict where the catagory is the key and the ctid#'s (along with the other included
-    information) is the value.
-    :return:
-    """
-    val_dict[category_var.get()] = value_var.get()
-    set_list()
 
-def set_list():
-    """
-    Defines the listbox that displays the keys & values of the val_dict.
-    :return:
-    """
-    listbox1.delete(0, END)
-    listbox2.delete(0, END)
-    for key in val_dict.keys():
-        listbox1.insert(END, key)
-        listbox2.insert(END, val_dict[key])
+class App(tk.Frame):
+    def __init__(self, master, dictionary):
+        """
+        Initializes the main frame which will be known as self
+        :param master:
+        :param dictionary:
+        :return:
+        """
+        tk.Frame.__init__(self, master)
 
-def process():
-    """
-    Calls the WriteFile function, which converts the val_dict into an excel file and prints the name the document will
-    be given to the terminal.
-    :return:
-    """
-    name = agency_var.get() + " " + grant_var.get() + " " + hp_rrh_var.get() + ".xls"
-    print name
-    WriteFile(str(name), val_dict)
+        self.dict = dictionary
+        self.values_dict = {}
 
-def clear_all():
-    """
-    Clears the values from the listboxes and the val_dict allowing a new report to be processed without restarting the
-    application.
-    :return:
-    """
-    val_dict.clear()
-    listbox1.delete(0, END)
-    listbox2.delete(0, END)
+        self.grant_choices = ["C15", "ZZ-127"]
+        self.hp_or_rrh = ["HP", "RRH"]
+        self.reporting_categories = sorted(["Choose a reporting category",
+                                            "Name",
+                                            "Social Security Number",
+                                            "Date of Birth",
+                                            "Race",
+                                            "Ethnicity",
+                                            "Gender",
+                                            "Veteran Status",
+                                            "Disabling Condition",
+                                            "Homeless Living Situation",
+                                            "Project Entry Date",
+                                            "Project Exit Date",
+                                            "Destination",
+                                            "Personal ID",
+                                            "Household ID",
+                                            "Relationship to Head of Household",
+                                            "Client Location",
+                                            "Length of Time on Street",
+                                            "Income Sources (Entry)",
+                                            "Income Sources (Exit)",
+                                            "Non-Cash Benefits (Entry)",
+                                            "Non-Cash Benefits (Exit)",
+                                            "Insurance (Entry)",
+                                            "Insurance (Exit)",
+                                            "Services Provided",
+                                            "Financial Assistance Provided",
+                                            "Move-In Date",
+                                            "Non-Homeless Living Situation",
+                                            "Date of Move-In",
+                                            "Year Entered Military Service",
+                                            "Year Separated from Military Service",
+                                            "Theaters of Operation",
+                                            "Military Branch",
+                                            "Discharge Status",
+                                            "Household Income as a % of AMI",
+                                            "Last Permanent Address",
+                                            "Total Monthly Income (Entry)",
+                                            "Total Monthly Income (Exit)",
+                                            "Continuously Homeless One Year",
+                                            "Times Homeless Past Three Years",
+                                            "No Head of Household",
+                                            "Residence Prior to Project Entry",
+                                            "Multiple Heads of Household",
+                                            "Very High Monthly Income (Entry)",
+                                            "Very High Monthly Income (Exit)"
+                                            ])
 
-root = Tk()
-root.title("SSVF Repository DQ Processor")
+        self.state = tk.StringVar(self)
+        self.provider = tk.StringVar(self)
+        self.hp_rrh = tk.StringVar(self)
+        self.grant = tk.StringVar(self)
+        self.reporting_category = tk.StringVar(self)
+        self.value = tk.StringVar(self)
 
-agency_var = StringVar()
-grant_var = StringVar()
-hp_rrh_var = StringVar()
-category_var = StringVar()
-value_var = StringVar()
+        self.state.trace("w", self.update_options)
 
-mainframe = ttk.Frame(root, padding="3 3 3 3")
-mainframe.grid(column=1, row=1, sticky=(N, W, S, E))
-mainframe.rowconfigure(0, weight=1)
-mainframe.columnconfigure(0, weight=1)
+        self.state_menu = tk.OptionMenu(self, self.state, *self.dict.keys())
+        self.provider_menu = tk.OptionMenu(self, self.provider, "")
+        self.hp_rrh_menu = tk.OptionMenu(self, self.hp_rrh, *self.hp_or_rrh)
+        self.grant_menu = tk.OptionMenu(self, self.grant, *self.grant_choices)
+        self.reporting_categories_menu = tk.OptionMenu(self, self.reporting_category, *self.reporting_categories)
 
-lower_frame = ttk.Frame(root, padding="3 3 3 3")
-lower_frame.grid(column=1, row=4, sticky=(N, W, S, E))
-lower_frame.rowconfigure(0, weight=1)
-lower_frame.columnconfigure(0, weight=1)
+        self.add_button = ttk.Button(self, command=self.addition, text="Add")
 
-reporting_categories = sorted(["Choose a reporting category",
-                               "Name",
-                               "Social Security Number",
-                               "Date of Birth",
-                               "Race",
-                               "Ethnicity",
-                               "Gender",
-                               "Veteran Status",
-                               "Disabling Condition",
-                               "Homeless Living Situation",
-                               "Project Entry Date",
-                               "Project Exit Date",
-                               "Destination",
-                               "Personal ID",
-                               "Household ID",
-                               "Relationship to Head of Household",
-                               "Client Location",
-                               "Length of Time on Street",
-                               "Income Sources (Entry)",
-                               "Income Sources (Exit)",
-                               "Non-Cash Benefits (Entry)",
-                               "Non-Cash Benefits (Exit)",
-                               "Insurance (Entry)",
-                               "Insurance (Exit)",
-                               "Services Provided",
-                               "Financial Assistance Provided",
-                               "Move-In Date",
-                               "Non-Homeless Living Situation",
-                               "Date of Move-In",
-                               "Year Entered Military Service",
-                               "Year Separated from Military Service",
-                               "Theaters of Operation",
-                               "Military Branch",
-                               "Discharge Status",
-                               "Household Income as a % of AMI",
-                               "Last Permanent Address",
-                               "Total Monthly Income (Entry)",
-                               "Total Monthly Income (Exit)",
-                               "Continuously Homeless One Year",
-                               "Times Homeless Past Three Years",
-                               "No Head of Household",
-                               "Residence Prior to Project Entry",
-                               "Multiple Heads of Household",
-                               "Very High Monthly Income (Entry)",
-                               "Very High Monthly Income (Exit)"])
+        self.y_scroll = tk.Scrollbar(self, orient="vertical")
+        self.list_box1 = tk.Listbox(self, width=30, yscrollcommand=self.y_scroll.set)
+        self.list_box2 = tk.Listbox(self, width=30, yscrollcommand=self.y_scroll.set)
+        self.y_scroll["command"] = self.list_box1.yview
+        self.y_scroll["command"] = self.list_box2.yview
 
-agency_choices = ["-",
-                  "Central City Concern",
-                  "Human Solutions",
-                  "Impact North West",
-                  "NAYA",
-                  "North West Pilot Projects",
-                  "Transition Projects"]
+        self.state.set("Select Your State")
+        self.provider.set("Select Your Provider")
+        self.grant.set("Select Your Grant")
+        self.hp_rrh.set("Select HP or RRH")
+        self.reporting_category.set("Choose the Reporting Category")
 
-grant_choices = ["-", "C15", "ZZ-127"]
+        self.state_menu.pack(side="top")
+        self.provider_menu.pack(side="top")
+        self.hp_rrh_menu.pack(side="top")
+        self.grant_menu.pack(side="top")
+        self.add_button.pack()
+        self.y_scroll.pack(side="bottom", fill="y")
+        self.list_box1.pack(side="bottom")
+        self.list_box2.pack(side="bottom")
+        self.pack()
 
-hp_or_rrh = ["-", "HP", "RRH"]
+    def addition(self):
+        """
+        Adds a new value to the val_dict where the catagory is the key and the ctid#'s (along with the other included
+        information) is the value.
+        :return:
+        """
+        self.values_dict[self.reporting_category.get()] = self.value.get()
+        self.set_list()
 
-ttk.Label(mainframe, text="Agency:").grid(column=1, row=1, sticky=N)
-ttk.Label(mainframe, text="Grant:").grid(column=2, row=1, sticky=N)
-ttk.Label(mainframe, text="HP or RRH:").grid(column=3, row=1, sticky=N)
+    def set_list(self):
+        """
+        defines the listbox that displays the keys & values of the values_dict.
+        :return:
+        """
+        self.list_box1.delete(0, End)
+        self.list_box2.delete(0, End)
+        for key in self.values_dict.keys():
+            self.list_box1.insert(END, key)
+            self.list_box2.insert(END, self.values_dict[key])
 
-OptionMenu(mainframe, agency_var, *agency_choices).grid(column=1, row=2, sticky=N)
-agency_var.set("-")
-OptionMenu(mainframe, grant_var, *grant_choices).grid(column=2, row=2, sticky=N)
-grant_var.set("-")
-OptionMenu(mainframe, hp_rrh_var, *hp_or_rrh).grid(column=3, row=2, sticky=N)
-hp_rrh_var.set("-")
+    def update_options(self, *args):
+        """
+        This method allows the provider menu to update depending on which state is selected.
+        :param args:
+        :return:
+        """
+        countries = self.dict[self.state.get()]
+        self.provider.set(countries[0])
 
-OptionMenu(mainframe, category_var, *reporting_categories).grid(column=1, row=3, sticky=(N, W))
-ttk.Entry(mainframe, textvariable=value_var).grid(column=2, row=3, sticky=(N, W, E))
-b = ttk.Button(mainframe, command=addition, text="Add")
-b.grid(column=3, row=3, sticky=(N, E))
+        menu = self.provider_menu["menu"]
+        menu.delete(0, 'end')
 
-y_scroll = Scrollbar(lower_frame, orient=VERTICAL)
-y_scroll.grid(column=3, row=0, sticky=(N, S))
-listbox1 = Listbox(lower_frame, width=30, yscrollcommand=y_scroll.set)
-listbox1.grid(column=1, row=0, sticky=(N, W, E))
-listbox2 = Listbox(lower_frame, width=30, yscrollcommand=y_scroll.set)
-listbox2.grid(column=2, row=0, sticky=(N, W, E))
-y_scroll["command"] = listbox1.yview
-y_scroll["command"] = listbox2.yview
+        for country in countries:
+            menu.add_command(label=country, command=lambda nation=country: self.provider.set(nation))
 
-ttk.Button(lower_frame, command=process, text="Process").grid(column=3, row=1, sticky=W)
-ttk.Button(lower_frame, command=clear_all, text="Clear Data").grid(column=2, row=1, sticky=W)
 
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    make_dict = Get_Providers.MakeDictionary()
+    app = App(root, make_dict.read_csv())
+    app.mainloop()
