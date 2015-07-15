80ac63a33607af1a9f04b6e0c807043c0068b7db
David Katz-Wigmore <katzwigmore@gmail.com>
2015-07-15 06:39:51 -0400

    Update RepositoryDQProcessor.py
    Made several behind the scenes changes to make the GUI more appealing and more responsive.

--- RepositoryDQProcessor.py
+++ RepositoryDQProcessor.py
@@ -17,6 +17,10 @@ class App(tk.Frame):
         """
         tk.Frame.__init__(self, master)
 
+        self.grid(column=0, row=1, sticky="N,W,S,E")
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
         self.dict = dictionary
         self.values_dict = {}
 
@@ -84,13 +88,24 @@ class App(tk.Frame):
         self.grant_menu = tk.OptionMenu(self, self.grant, *self.grant_choices)
         self.reporting_categories_menu = tk.OptionMenu(self, self.reporting_category, *self.reporting_categories)
 
+        self.values = ttk.Entry(self, width=48, textvariable=self.value)
         self.add_button = ttk.Button(self, command=self.addition, text="Add")
 
-        self.y_scroll = tk.Scrollbar(self, orient="vertical")
-        self.list_box1 = tk.Listbox(self, width=30, yscrollcommand=self.y_scroll.set)
-        self.list_box2 = tk.Listbox(self, width=30, yscrollcommand=self.y_scroll.set)
-        self.y_scroll["command"] = self.list_box1.yview
-        self.y_scroll["command"] = self.list_box2.yview
+        self.tree = ttk.Treeview(self,
+                                 columns=("Reporting Category", "Error Values"),
+                                 selectmode="extended",
+                                 show="headings"
+                                 )
+        self.tree.column("#0", width=10)
+        self.tree.column("Reporting Category", width=50)
+        self.tree.heading(0, text="Reporting Category")
+        self.tree.heading(1, text="Error Values")
+
+        self.y_scroll = ttk.Scrollbar(self, orient="vertical", command=self.tree.yview)
+        self.tree.configure(yscrollcommand=self.y_scroll.set)
+
+        self.clear = ttk.Button(self, command=self.clear_all(), text="Clear All")
+        self.process_button = ttk.Button(self, command=self.process(), text="Process")
 
         self.state.set("Select Your State")
         self.provider.set("Select Your Provider")
@@ -98,35 +113,60 @@ class App(tk.Frame):
         self.hp_rrh.set("Select HP or RRH")
         self.reporting_category.set("Choose the Reporting Category")
 
-        self.state_menu.pack(side="top")
-        self.provider_menu.pack(side="top")
-        self.hp_rrh_menu.pack(side="top")
-        self.grant_menu.pack(side="top")
-        self.add_button.pack()
-        self.y_scroll.pack(side="bottom", fill="y")
-        self.list_box1.pack(side="bottom")
-        self.list_box2.pack(side="bottom")
-        self.pack()
+        self.state_menu.grid(column=0, row=1, padx=2, sticky="E")
+        self.provider_menu.grid(column=1, row=1, padx=2, sticky="E")
+        self.grant_menu.grid(column=2, row=1, padx=2, sticky="W")
+        self.hp_rrh_menu.grid(column=3, row=1, padx=2, sticky="w")
+        self.reporting_categories_menu.grid(column=0, row=2, padx=2, sticky="E")
+        self.values.grid(column=1, columnspan=2, row=2, sticky="W, E")
+        self.add_button.grid(column=3, row=2, sticky="W")
+        self.tree.grid(column=0, columnspan=4, row=3, sticky="E, W")
+        self.y_scroll.grid(column=4, row=3, sticky="N, W, S")
+        self.clear.grid(column=2, row=4, sticky="E")
+        self.process_button.grid(column=3, row=4, sticky="E")
 
     def addition(self):
         """
-        Adds a new value to the val_dict where the catagory is the key and the ctid#'s (along with the other included
+        Adds a new value to the val_dict where the category is the key and the ctid#'s (along with the other included
         information) is the value.
         :return:
         """
         self.values_dict[self.reporting_category.get()] = self.value.get()
         self.set_list()
 
+    def clear_all(self):
+        """
+        Clears all the values from the tree widget and the value_dict allowing a new report to be processed
+        without closing and re-opening the program.
+        :return:
+        """
+        self.values_dict.clear()
+        for child_id in self.tree.get_children():
+            self.tree.delete(child_id)
+
+    def process(self):
+        """
+        Calls the WriteFile function, which converts the value_dict into an excel file.
+        The name of this file will be printed to the terminal window to show that the process
+        is complete
+        :return:
+        """
+        name = self.provider.get() + " " + self.grant.get() + " " + self.hp_rrh.get() + ".xls"
+        try:
+            WriteFile(str(name), self.values_dict)
+            print(name)
+        except ValueError:
+            print("Error! File not saved.")
+
     def set_list(self):
         """
         defines the listbox that displays the keys & values of the values_dict.
         :return:
         """
-        self.list_box1.delete(0, End)
-        self.list_box2.delete(0, End)
+        for child_id in self.tree.get_children():
+            self.tree.delete(child_id)
         for key in self.values_dict.keys():
-            self.list_box1.insert(END, key)
-            self.list_box2.insert(END, self.values_dict[key])
+            self.tree.insert("", index="end", values=(key, self.values_dict[key]))
 
     def update_options(self, *args):
         """
@@ -134,14 +174,13 @@ class App(tk.Frame):
         :param args:
         :return:
         """
-        countries = self.dict[self.state.get()]
-        self.provider.set(countries[0])
+        providers = self.dict[self.state.get()]
 
         menu = self.provider_menu["menu"]
         menu.delete(0, 'end')
 
-        for country in countries:
-            menu.add_command(label=country, command=lambda nation=country: self.provider.set(nation))
+        for provider in providers:
+            menu.add_command(label=provider, command=lambda region=provider: self.provider.set(region))
 
 
 if __name__ == "__main__":
